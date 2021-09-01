95a017d91c27bf48736260505f19321f6d143b2d
MarinaTripetska <marina.tripetskaya@gmail.com>
2021-09-01 11:31:49 +0200

    виплавляє баг ползунка

--- src/index.js
+++ src/index.js
@@ -1,78 +1,34 @@
 import './sass/main.scss';
-
 import refs from './refs/refs_html.js'
 const { body, ul, input } = refs;
-// console.log(ul);
-
 import data from './menu.json'
-// console.log(data);
-
 import template from './templates/menu.hbs'
-// console.log(template);
-
-
-
 //Розмітка
 const item = template(data)
-// console.log(item);
-
 ul.insertAdjacentHTML('beforeend', item)
-
-
 //Переключення теми
-
-
 const theme = {
   light: 'light-theme',
   dark: 'dark-theme',
 };
 const {light, dark} = theme;
-  
-savedTheme()
-input.addEventListener('change', (e) => {
-        
+ 
+body.classList.add(
+  localStorage.getItem('theme') === null ? light : localStorage.getItem('theme')
+);
+if (body.classList.value === dark) {
+    input.checked = true;
+};
+
+input.addEventListener('change', () => {
     if (input.checked === true) {
-        
         body.classList.remove(light)
         body.classList.add(dark)
-
-        localStorage.setItem('class', dark);
-        localStorage.setItem('checked', true);
-
-        let lockalBool = localStorage.getItem('checked');
-
-        input.setAttribute('checked', lockalBool)
-    
-    }
-    if (input.checked===false) {
+        localStorage.setItem('theme', dark);
+        }
+    if (input.checked === false) {
         body.classList.remove(dark);
         body.classList.add(light);
-        
-        localStorage.setItem('class', light);
-        localStorage.setItem('checked', false);
-        let lockalBool = localStorage.getItem('checked');
-
-        input.setAttribute('checked',lockalBool)
-       }
-
+        localStorage.setItem('theme', light);
+        }
 });
-
-
-
-
-function savedTheme() {
-   let className = localStorage.getItem('class');
-    let lockalBool = localStorage.getItem('checked');
-    const bodyCurrentClass = body.className
-    console.log(bodyCurrentClass);
-    if (lockalBool !== null) {
-       
-        input.setAttribute('checked', lockalBool)
-         body.classList.add(className)
-
-    }
-    
-}
-
-
-
